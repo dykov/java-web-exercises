63bb8450d03aa46cc1f01320628404662d97544c
dykov <dykovoleksii@gmail.com>
2023-03-19 13:03:57 +0200

    Complete socket exercise

--- 1-0-networking-and-http/1-0-0-hello-network-socket/src/main/java/com/bobocode/net/server/MessageBoardServer.java
+++ 1-0-networking-and-http/1-0-0-hello-network-socket/src/main/java/com/bobocode/net/server/MessageBoardServer.java
@@ -1,8 +1,15 @@
 package com.bobocode.net.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
 
 import static com.bobocode.net.server.ServerUtil.*;
 
@@ -17,15 +24,36 @@ import static com.bobocode.net.server.ServerUtil.*;
 public class MessageBoardServer {
     public static final String HOST = ServerUtil.getLocalHost();
     public static final int PORT = 8899; // you can use any free port you want
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     public static void main(String[] args) throws IOException {
-        try (ServerSocket serverSocket = createServerSocket(PORT)) {
-            while (true) {
-                try (Socket clientSocket = acceptClientSocket(serverSocket)) {
-                    String message = readMessageFromSocket(clientSocket);
+        try(ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while(true) {
+                try(
+                        final Socket clientSocket = serverSocket.accept();
+                        final InputStreamReader reader = new InputStreamReader(clientSocket.getInputStream());
+                        final BufferedReader bufferedReader = new BufferedReader(reader)
+                ) {
+                    final String message = bufferedReader.lines().collect(Collectors.joining());
+
                     printMessage(clientSocket, message);
                 }
             }
         }
     }
+
+    /**
+     * Simple message that allows to print message using a nice and informative format. Apart from the text message
+     * itself, it also prints the time and client address.
+     *
+     * @param socket  accepted socket
+     * @param message a text message to print
+     */
+    public static void printMessage(Socket socket, String message) {
+        final String time = LocalDateTime.now().format(TIME_FORMATTER);
+        String clientAddress = socket.getInetAddress().getHostAddress();
+        final String messageData = String.format("[%s][%s]: %s", time, clientAddress, message);
+        System.out.println(messageData);
+    }
+
 }
